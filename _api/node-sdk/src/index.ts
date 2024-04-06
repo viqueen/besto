7f708c2e500b14d7ba1de191051e29f57358d20b
viqueen <hasnae@viqueen.org>
2024-04-07 07:49:07 +1000

    move things out of identity v1 for now

--- _api/node-sdk/src/index.ts
+++ _api/node-sdk/src/index.ts
@@ -1,2 +1,4 @@
 export * from "./identity/v1/models";
 export * from "./identity/v1/identity_service";
+export * from "./platform/profile/v1/models";
+export * from "./platform/profile/v1/profile_service";
--- _api/web-sdk/src/index.ts
+++ _api/web-sdk/src/index.ts
@@ -1,3 +1,6 @@
 export * from "./identity/v1/models_pb";
 export * from "./identity/v1/identity_service_pb";
 export * from "./identity/v1/identity_service_connect";
+export * from "./platform/profile/v1/models_pb";
+export * from "./platform/profile/v1/profile_service_pb";
+export * from "./platform/profile/v1/profile_service_connect";
